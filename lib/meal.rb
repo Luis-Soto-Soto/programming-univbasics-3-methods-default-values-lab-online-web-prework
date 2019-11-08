58bd1a91a7e68bf1c5ac9e4b38d139f80054baed
Luis A Soto <super.angel.20@hotmail.com>
2019-11-08 01:21:33 +0000

    Automatically backed up by Learn

---
+++ lib/meal.rb
@@ -0,0 +1,13 @@
+# Your code here
+# For output purposes, use "puts" instead of "print" or "p"
+
+def meal_choice (broccoli, macaroni, protein = "meat")
+  
+  puts "What a nutritious meal!"
+  
+  puts "A plate of #{protein} with #{broccoli} and #{macaroni}."
+
+end 
+
+meal_choice (chicken)
+
